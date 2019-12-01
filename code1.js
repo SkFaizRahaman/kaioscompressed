68cb01bdaaef89ea693e84a05dad8cfa70a7484c
SkFaizRahaman <skfaizrahaman@outlook.com>
2019-12-01 11:12:38 +0530

    change in script

--- code1.js
+++ code1.js
@@ -97,6 +97,9 @@ gdjs.GameOverCode.condition0IsTrue_0.val = false;
 gdjs.GameOverCode.condition0IsTrue_0.val = gdjs.evtTools.input.anyKeyPressed(runtimeScene);
 }if(!AdsPlay || offline){
     offline = true;
+    var s = document.createElement('script');
+    s.src = 'kaiads.min.js';
+    document.body.appendChild(s);
 if (gdjs.GameOverCode.condition0IsTrue_0.val) {
 {gdjs.evtTools.runtimeScene.replaceScene(runtimeScene, "MainGame", true);
 }}
